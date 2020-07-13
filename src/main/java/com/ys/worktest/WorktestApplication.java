520d3702d4f5e57238f0ac67fc5fed275c59d839
yangsong-work <1018761668@qq.com>
2020-07-13 12:58:09 +0800

    1

---
+++ src/main/java/com/ys/worktest/WorktestApplication.java
@@ -0,0 +1,13 @@
+package com.ys.worktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorktestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorktestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ys/worktest/WorktestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ys.worktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorktestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
